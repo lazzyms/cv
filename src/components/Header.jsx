1950847b3cae671e41f63fd1bd45902f1c5b319f
lazzyms <maulik.sompura06@gmail.com>
2020-12-30 16:16:50 +0530

    share button added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
@@ -11,9 +11,29 @@ import {
   FaSkype,
   FaLinkedin,
 } from "react-icons/fa";
+import { FiShare } from "react-icons/fi";
 import { DiStackoverflow } from "react-icons/di";
 
 function Header(props) {
+  const [copied, setCopied] = useState(false);
+  let shareProfile = () => {
+    let bondUrl = window.location.href;
+    console.log(bondUrl);
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "Portfolio | M",
+          url: bondUrl,
+        })
+        .then(() => {
+          console.log("Thanks for sharing!");
+        })
+        .catch(console.error);
+    } else {
+      navigator.clipboard.writeText(bondUrl);
+      setCopied(true);
+    }
+  };
   return (
     <Row className="justify-content-center align-items-center">
       <Col md="12" sm="12" className="mb-3 text-center">
@@ -21,39 +41,34 @@ function Header(props) {
       </Col>
       <Col md="12" sm="12">
         <h1 className="text-light text-uppercase text-center font-weight-bold display-3">
-          {props.personal.name}
+          {props.personal.name}{" "}
+          
         </h1>
         <Row className="justify-content-center">
-            <Button
-              variant="light"
-              href={"mailto:" + props.personal.email}
-            >
-              <FaEnvelope />
-            </Button>
-            <Button variant="light" href={props.personal.blog}>
-              <FaBloggerB />
-            </Button>
-            <Button
-              variant="light"
-              href={"tel:" + props.personal.phone}
-            >
-              <FaPhoneAlt />
-            </Button>
-            <Button variant="light" href={props.personal.github}>
-              <FaGithub />
-            </Button>
-            <Button variant="light" href={props.personal.skype}>
-              <FaSkype />
-            </Button>
-            <Button variant="light" href={props.personal.linkedIn}>
-              <FaLinkedin />
-            </Button>
-            <Button
-              variant="light"
-              href={props.personal.stackoverflow}
-            >
-              <DiStackoverflow />
-            </Button>
+          <Button variant="light" href={"mailto:" + props.personal.email}>
+            <FaEnvelope />
+          </Button>
+          <Button variant="light" href={props.personal.blog}>
+            <FaBloggerB />
+          </Button>
+          <Button variant="light" href={"tel:" + props.personal.phone}>
+            <FaPhoneAlt />
+          </Button>
+          <Button variant="light" href={props.personal.github}>
+            <FaGithub />
+          </Button>
+          <Button variant="light" href={props.personal.skype}>
+            <FaSkype />
+          </Button>
+          <Button variant="light" href={props.personal.linkedIn}>
+            <FaLinkedin />
+          </Button>
+          <Button variant="light" href={props.personal.stackoverflow}>
+            <DiStackoverflow />
+          </Button>
+          <Button variant="outline-light" onClick={shareProfile}>
+            <FiShare /> {copied ? 'Link Copied' : 'Share Profile Link'}
+          </Button>
         </Row>
       </Col>
     </Row>
